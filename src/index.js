82a3b7161cb134c6aa1593880bd5654fd7d0bdb1
Karen <karenchakhalyan23@gmail.com>
2021-03-09 23:41:26 +0400

    some changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom"
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
 
+        this.state = { lat: null, errorMessage: '' }
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude})
+            },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+        
+        )
+    }
+
+    render() {
+        return (
+            <>
+            <div>Latitude: {this.state.lat}</div>
+            <br />
+            <div>Error: {this.state.errorMessage}</div>
+            </>
+        );
+    }
 }
 
-export default App;
+ReactDOM.render(<App/>, document.querySelector('#root'));
