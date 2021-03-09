920a091012c9f287c35a8c8fd58d2de252aebaf5
Karen <karenchakhalyan23@gmail.com>
2021-03-10 00:29:11 +0400

    some changes

--- src/index.js
+++ src/index.js
@@ -2,30 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom"
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
+        state = { lat: null, errorMessage: '' }
 
-        this.state = { lat: null, errorMessage: '' }
-
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude})
-            },
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
-        
-        )
-    }
-
-    render() {
-        return (
-            <>
-            <div>Latitude: {this.state.lat}</div>
-            <br />
-            <div>Error: {this.state.errorMessage}</div>
-            </>
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message })
         );
+    }
+    render() {
+         
+            if(this.state.errorMessage && !this.state.lat){
+                return <div>Error: {this.state.errorMessage}</div>
+            }else if(this.state.lat && !this.state.errorMessage){
+                return <div>Latitude: {this.state.lat}</div>
+            }else{
+                return <div>Loading...</div>
+            }    
+        
     }
 }
 
